982e772469d1abb85826e6a75a90ae1bad061036
Christoffer Ventus <christoffer.ventus@gmail.com>
2021-02-08 21:37:05 +0200

    Add small DI-functionality
    inject.ts provides a small type-checked dependency injection framework
    with few bells and whistles. Unlike Angular/Nest.js/injection-js and
    some others, the type and name of injectables are not tied to classes.
    Rather, a names are always strings and a value of any type can be a
    depenency. tsc can statically ensure that all dependencies can be
    resolved and no run-time errors can appear.
    
    The `inject()` function can be used to both declare interfaces for
    abstract services and to define providers for services. For instance,
    configuration can be declared with
    
        interface Config {
           port: number
           env: 'production' | 'development'
        }
        //     name            type
        const Config = inject<Config>()
    
    Other services should preferably agree on the name of injectables, in
    this case "Config", but it's not strictly required. A service that
    depends on the confiugration is defined as follows:
    
        interface MyServiceDependencies {
            Config: Config,
            Logger: SomeLogger,
            ...
        }
        //                           injectables (e.g. classes)
        const MyService = inject({ Config, Logger }, services => {
          //      realized values (instances)
          const { Config, Logger } = services
    
          const myService = ...
    
          // Return a value or a Promise
          return myService
        })
    
    The first argument specifies the dependencies, using JavaScript's
    convenient short-hand notation for `{ Config: Config, ... }`. The
    parameter type of the constructor that follows is similar to the
    dependency declaration, with values of the injected type.
    
    With one or more services it's possible to string them together,
    typically at application startup, using the `assemble()` function:
    
        const MyAsyncLogger = inject<Logger>(...)
        const myConfig: Config = { ... }
        const assembly = await assemble({
            Config: inject(myConfig),
            MyService,
            MyOtherService,
            Logger: MyAsyncLogger,
            ...
        })
    
    There needs to be a provider for every dependency and since the type
    of each provider is rich enough you get a type error if some dependency
    cannot be provided. Mutual or circular dependencies are not allowed and
    result in a run-time error, however.
    
    As seen above, the `inject()` function can also be called with a single
    argument, in which case it creates a constant value provider.
    
    A third parameter to `inject()` defines a cleanup function, which
    receives a single parameter - the value returned from the constructor.

---
+++ src/inject.ts
@@ -0,0 +1,127 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+declare const type: unique symbol
+
+export interface Injectable<Type> {
+  readonly [type]: Type
+}
+
+export type InjectableType<
+  I extends Injectable<unknown>
+> = I extends Injectable<infer T> ? T : never
+
+type ResolvedDependencies<D extends Record<string, Injectable<unknown>>> = {
+  [key in string & keyof D]: D[key] extends Injectable<infer Type>
+    ? Type
+    : never
+}
+
+export interface Provider<
+  Type,
+  Dependencies extends Record<string, Injectable<unknown>>
+> extends Injectable<Type> {
+  readonly dependencies: Dependencies
+  readonly create: (
+    resolved: ResolvedDependencies<Dependencies>,
+  ) => Type | Promise<Type>
+  readonly destroy?: (value: Type) => void | Promise<void>
+}
+
+interface Inject {
+  <Type>(): Injectable<Type>
+  <Type>(value: Type): Provider<Type, Record<string, never>>
+  <Type, Dependencies extends Record<string, Injectable<unknown>>>(
+    dependencies: Dependencies,
+    create: (
+      resolved: ResolvedDependencies<Dependencies>,
+    ) => Type | Promise<Type>,
+    destroy?: (value: Type) => void | Promise<void>,
+  ): Provider<Type, Dependencies>
+}
+
+export const inject: Inject = function inject(
+  value?: unknown,
+  create?: unknown,
+  destroy?: unknown,
+) {
+  if (arguments.length === 0) {
+    return {}
+  } else if (create) {
+    return { dependencies: value, create, destroy }
+  } else {
+    return { dependencies: {}, create: () => value } as any
+  }
+}
+
+type AssemblyTypes<Providers extends Record<string, Provider<any, any>>> = {
+  [key in string & keyof Providers]: InjectableType<Providers[key]>
+}
+
+type CompleteAssembly<
+  Providers extends Record<string, Provider<any, any>>
+> = Record<
+  keyof Providers,
+  { create: (resolved: AssemblyTypes<Providers>) => unknown }
+>
+
+interface Assembly<Providers extends Record<string, Provider<any, any>>> {
+  get<Name extends keyof Providers>(name: Name): InjectableType<Providers[Name]>
+  destroy(): Promise<void>
+}
+
+export async function assemble<
+  Providers extends Record<string, Provider<any, any>>
+>(
+  providers: Providers & CompleteAssembly<Providers>,
+): Promise<Assembly<Providers>> {
+  type Name = keyof Providers
+  type ComponentMap = Partial<Record<Name, any>>
+
+  const providerNames: Name[] = Object.keys(providers)
+
+  const visited: Set<Name> = new Set()
+  const components: ComponentMap = {}
+  let destroyables: (() => Promise<void>)[] = []
+
+  const build = async (name: Name) => {
+    if (components[name]) {
+      return components[name]
+    }
+    if (visited.has(name)) {
+      throw new Error('assemble: circular dependency')
+    }
+    visited.add(name)
+
+    const { create, dependencies, destroy } = providers[name]
+
+    const param: ComponentMap = {}
+    for (const dependency of Object.keys(dependencies)) {
+      param[dependency as Name] = await build(dependency)
+    }
+
+    const component = await Promise.resolve(create(param))
+    components[name] = component
+
+    if (destroy) {
+      // Prepend to list so that in-order traveral destroys components in FILO order
+      destroyables.unshift(() => Promise.resolve(destroy(components)))
+    }
+
+    return component
+  }
+
+  for (const dependency of providerNames) {
+    await build(dependency)
+  }
+
+  return {
+    get: (name: Name) => components[name],
+    destroy: async () => {
+      const list = destroyables
+      destroyables = []
+      for (const destroyCallback of list) {
+        await destroyCallback()
+      }
+    },
+  }
+}
---
+++ test/inject.spec.ts
@@ -0,0 +1,27 @@
+import { inject, assemble } from '../src/inject'
+
+// Declare injectable Colors:string[]
+const Colors = inject<string[]>()
+
+// Define constant provider Name:string
+const Name = inject('John Doe')
+
+// Define provider with dependencies
+const Message = inject({ Name, Colors }, ({ Name: name, Colors: colors }) => {
+  return `Hello! My name is ${name}. I like ${colors.join(', ')}.`
+})
+
+describe('assemble', () => {
+  it('should assemble providers into an assembly', async () => {
+    const assembly = await assemble({
+      Name,
+      Message,
+      // Define a provider for Color:string[]
+      Colors: inject(['red', 'green', 'blue']),
+    })
+
+    const message = assembly.get('Message')
+
+    expect(message).toBe('Hello! My name is John Doe. I like red, green, blue.')
+  })
+})
