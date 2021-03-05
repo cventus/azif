e9594ebdccf91becb780f04b3442737e0fd73c23
Christoffer Ventus <christoffer.ventus@gmail.com>
2021-03-05 23:25:57 +0200

    Rename SocketsService -> SessionsService

---
+++ src/server/sessions/SessionsService.ts
@@ -0,0 +1,17 @@
+import { inject } from '../../inject'
+import { User } from '../users'
+
+export interface SocketSession {
+  id: string
+  userId: string
+  gameId?: string
+}
+
+export interface SessionsService {
+  createSession(sessionId: string, userId: string): Promise<SocketSession | undefined>
+  getSession(sessionId: string): Promise<SocketSession | undefined>
+  subscribeToGame(sessionId: string, gameId: string): Promise<void>
+  destroySession(sessionId: string): Promise<void>
+}
+
+export const SessionsService = inject<SessionsService>()
